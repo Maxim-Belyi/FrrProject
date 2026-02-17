2abb1994c190123e68edb5d8a2d3c32005e1d669
Maxim Belyi <btf9458@gmail.com>
2026-02-17 21:12:22 +0500

    change init.php

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,29 +1,11 @@
 <?php
-	if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
-	{
-		die('gtfo');
-	}
+require_once(__DIR__ . '/../../vendor/autoload.php');
 
-	if (!defined('FROM_XPAGE_OFFICE'))
-	{
-		define('FROM_XPAGE_OFFICE', $_SERVER['REMOTE_ADDR'] === '94.181.33.167');
-	}
-	define('LOCAL_DEPLOYMENT', getenv('ENVIRONMENT') === 'dl');
+try {
+    \Xpage\Local::registerEventsHandlers();
+} catch (\Throwable $e) {
+    \Xpage\Tools::log($e, 'init', 'errors');
 
-//Автолоад классов из папки /local/php_interface/classes
-	\Bitrix\Main\Loader::registerNamespace("Xpage", $_SERVER['DOCUMENT_ROOT'] . "/src/classes/Xpage");
-	\Bitrix\Main\Loader::registerNamespace("Project", $_SERVER['DOCUMENT_ROOT'] . "/src/classes/Project");
-	try
-	{
-		/*Здесь регистрируй события*/
-		\Xpage\Local::registerEventsHandlers();
-	}
-	catch (\Throwable $e)
-	{
-		\Xpage\Tools::log($e, 'init', 'errors');
-	}
-
-	//изначально проект закрывает контент ото всех
-	//\Xpage\Tools::closeByIp();
+}
 
 
